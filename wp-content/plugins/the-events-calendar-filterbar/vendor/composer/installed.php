8c6b9d77f54b61bf9465a37063c853eaa252a60c
Your Name <wermeson.digietal@gmail.com>
2024-07-20 09:08:17 -0300

    filto de categorias, retirada de calendario

---
+++ wp-content/plugins/the-events-calendar-filterbar/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/events-filterbar',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '74004220d0892c01f672b04d9a33ba4d42c89ba8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/events-filterbar' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '74004220d0892c01f672b04d9a33ba4d42c89ba8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
